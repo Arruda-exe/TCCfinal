3e32351176d0af18dd39282c750d49d941ddae95
arrudamygit@gmail.com <arrudamygit@gmail.com>
2025-06-09 09:30:05 -0300

    ta tudo dando certo

--- TCCfinal/Inicio.Designer.cs
+++ TCCfinal/Inicio.Designer.cs
@@ -38,19 +38,20 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.btnUser = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.radioButton1 = new System.Windows.Forms.RadioButton();
-            this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnCadastra = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
+            this.btnAtualizar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.txtData = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboDescricao = new System.Windows.Forms.ComboBox();
+            this.radioSaida = new System.Windows.Forms.RadioButton();
+            this.radioEntrada = new System.Windows.Forms.RadioButton();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -159,81 +160,84 @@
             // 
             // panel2
             // 
+            this.panel2.Controls.Add(this.btnCadastra);
             this.panel2.Controls.Add(this.label4);
-            this.panel2.Controls.Add(this.button4);
-            this.panel2.Controls.Add(this.button3);
-            this.panel2.Controls.Add(this.button2);
-            this.panel2.Controls.Add(this.textBox2);
-            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.btnAtualizar);
+            this.panel2.Controls.Add(this.btnExcluir);
+            this.panel2.Controls.Add(this.btnCadastrar);
+            this.panel2.Controls.Add(this.txtValor);
+            this.panel2.Controls.Add(this.txtData);
             this.panel2.Controls.Add(this.label3);
             this.panel2.Controls.Add(this.label2);
             this.panel2.Controls.Add(this.label1);
-            this.panel2.Controls.Add(this.comboBox1);
-            this.panel2.Controls.Add(this.radioButton2);
-            this.panel2.Controls.Add(this.radioButton1);
+            this.panel2.Controls.Add(this.comboDescricao);
+            this.panel2.Controls.Add(this.radioSaida);
+            this.panel2.Controls.Add(this.radioEntrada);
             this.panel2.Controls.Add(this.dataGridView1);
             this.panel2.Location = new System.Drawing.Point(326, 2);
             this.panel2.Name = "panel2";
             this.panel2.Size = new System.Drawing.Size(1308, 1024);
             this.panel2.TabIndex = 1;
             // 
-            // dataGridView1
+            // btnCadastra
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 115);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.RowTemplate.Height = 28;
-            this.dataGridView1.Size = new System.Drawing.Size(1015, 906);
-            this.dataGridView1.TabIndex = 0;
+            this.btnCadastra.Location = new System.Drawing.Point(1073, 381);
+            this.btnCadastra.Name = "btnCadastra";
+            this.btnCadastra.Size = new System.Drawing.Size(92, 50);
+            this.btnCadastra.TabIndex = 14;
+            this.btnCadastra.Text = "cadastrar";
+            this.btnCadastra.UseVisualStyleBackColor = true;
+            this.btnCadastra.Click += new System.EventHandler(this.btnCadastra_Click);
             // 
-            // radioButton1
+            // label4
             // 
-            this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(1076, 115);
-            this.radioButton1.Name = "radioButton1";
-            this.radioButton1.Size = new System.Drawing.Size(89, 24);
-            this.radioButton1.TabIndex = 1;
-            this.radioButton1.TabStop = true;
-            this.radioButton1.Text = "entrada";
-            this.radioButton1.UseVisualStyleBackColor = true;
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(47, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
             // 
-            // radioButton2
+            // btnAtualizar
             // 
-            this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(1189, 115);
-            this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(72, 24);
-            this.radioButton2.TabIndex = 2;
-            this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "saida";
-            this.radioButton2.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Location = new System.Drawing.Point(1134, 450);
+            this.btnAtualizar.Name = "btnAtualizar";
+            this.btnAtualizar.Size = new System.Drawing.Size(95, 53);
+            this.btnAtualizar.TabIndex = 11;
+            this.btnAtualizar.Text = "atualizar";
+            this.btnAtualizar.UseVisualStyleBackColor = true;
+            this.btnAtualizar.Click += new System.EventHandler(this.btnAtualizar_Click);
             // 
-            // comboBox1
+            // btnExcluir
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(1170, 185);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 28);
-            this.comboBox1.TabIndex = 3;
+            this.btnExcluir.Location = new System.Drawing.Point(1189, 381);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(92, 50);
+            this.btnExcluir.TabIndex = 10;
+            this.btnExcluir.Text = "excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
-            // label1
+            // btnCadastrar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1083, 188);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 20);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "descrição";
+            this.btnCadastrar.Location = new System.Drawing.Point(0, 0);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 15;
             // 
-            // label2
+            // txtValor
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1083, 256);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 20);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "data";
+            this.txtValor.Location = new System.Drawing.Point(1170, 310);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(121, 26);
+            this.txtValor.TabIndex = 8;
+            // 
+            // txtData
+            // 
+            this.txtData.Location = new System.Drawing.Point(1170, 253);
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(121, 26);
+            this.txtData.TabIndex = 7;
             // 
             // label3
             // 
@@ -244,55 +248,65 @@
             this.label3.TabIndex = 6;
             this.label3.Text = "valor";
             // 
-            // textBox1
+            // label2
             // 
-            this.textBox1.Location = new System.Drawing.Point(1170, 253);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(121, 26);
-            this.textBox1.TabIndex = 7;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1083, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "data";
             // 
-            // textBox2
+            // label1
             // 
-            this.textBox2.Location = new System.Drawing.Point(1170, 310);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(121, 26);
-            this.textBox2.TabIndex = 8;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1083, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "descrição";
             // 
-            // button2
+            // comboDescricao
             // 
-            this.button2.Location = new System.Drawing.Point(1070, 382);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(95, 49);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "cadastrar";
-            this.button2.UseVisualStyleBackColor = true;
+            this.comboDescricao.FormattingEnabled = true;
+            this.comboDescricao.Location = new System.Drawing.Point(1170, 185);
+            this.comboDescricao.Name = "comboDescricao";
+            this.comboDescricao.Size = new System.Drawing.Size(121, 28);
+            this.comboDescricao.TabIndex = 3;
             // 
-            // button3
+            // radioSaida
             // 
-            this.button3.Location = new System.Drawing.Point(1189, 381);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(92, 50);
-            this.button3.TabIndex = 10;
-            this.button3.Text = "excluir";
-            this.button3.UseVisualStyleBackColor = true;
+            this.radioSaida.AutoSize = true;
+            this.radioSaida.Location = new System.Drawing.Point(1189, 115);
+            this.radioSaida.Name = "radioSaida";
+            this.radioSaida.Size = new System.Drawing.Size(72, 24);
+            this.radioSaida.TabIndex = 2;
+            this.radioSaida.TabStop = true;
+            this.radioSaida.Text = "saida";
+            this.radioSaida.UseVisualStyleBackColor = true;
+            this.radioSaida.CheckedChanged += new System.EventHandler(this.radioSaida_CheckedChanged);
             // 
-            // button4
+            // radioEntrada
             // 
-            this.button4.Location = new System.Drawing.Point(1134, 450);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(95, 53);
-            this.button4.TabIndex = 11;
-            this.button4.Text = "atualizar";
-            this.button4.UseVisualStyleBackColor = true;
+            this.radioEntrada.AutoSize = true;
+            this.radioEntrada.Location = new System.Drawing.Point(1076, 115);
+            this.radioEntrada.Name = "radioEntrada";
+            this.radioEntrada.Size = new System.Drawing.Size(89, 24);
+            this.radioEntrada.TabIndex = 1;
+            this.radioEntrada.TabStop = true;
+            this.radioEntrada.Text = "entrada";
+            this.radioEntrada.UseVisualStyleBackColor = true;
+            this.radioEntrada.CheckedChanged += new System.EventHandler(this.radioEntrada_CheckedChanged);
             // 
-            // label4
+            // dataGridView1
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(47, 44);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(51, 20);
-            this.label4.TabIndex = 12;
-            this.label4.Text = "label4";
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 115);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1015, 906);
+            this.dataGridView1.TabIndex = 0;
             // 
             // Inicio
             // 
@@ -328,18 +342,19 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox4;
         private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.RadioButton radioButton2;
-        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioSaida;
+        private System.Windows.Forms.RadioButton radioEntrada;
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnAtualizar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.TextBox txtData;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboDescricao;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCadastra;
     }
 }
--- TCCfinal/Inicio.cs
+++ TCCfinal/Inicio.cs
@@ -7,17 +7,179 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace TCCfinal
 {
     public partial class Inicio : Form
     {
+        string connStr = "server=localhost;user=root;database=controle_financeiro;port=3306;password=;";
+
         public Inicio()
         {
             InitializeComponent();
-           
+            CarregarLancamentos();    // Carrega os lançamentos no DataGridView ao abrir o formulário
+            CarregarCategorias();     // Carrega as categorias no ComboBox ao abrir o formulário
+
         }
-      
+        // Carrega os lançamentos financeiros do banco de dados e exibe no DataGridView
+        private void CarregarLancamentos()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open(); // Abre a conexão com o banco
+                    string sql = "SELECT id, data, tipo, categoria, descricao, valor FROM lancamentos ORDER BY data DESC";
+                    MySqlDataAdapter da = new MySqlDataAdapter(sql, conn); // Prepara o comando SQL
+                    DataTable dt = new DataTable();
+                    da.Fill(dt); // Preenche o DataTable com os dados do banco
+                    dataGridView1.DataSource = dt; // Exibe os dados no DataGridView
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao carregar lançamentos: " + ex.Message);
+                }
+            }
+        }
+        // Carrega as categorias no ComboBox com base no tipo selecionado (entrada ou saída)
+        private void CarregarCategorias()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    // Define o tipo de categoria de acordo com o RadioButton selecionado
+                    string tipo = radioEntrada.Checked ? "receita" : "despesa";
+                    string sql = "SELECT nome FROM categorias WHERE tipo = @tipo";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+                    cmd.Parameters.AddWithValue("@tipo", tipo);
+                    MySqlDataReader dr = cmd.ExecuteReader();
+                    comboDescricao.Items.Clear(); // Limpa as opções anteriores
+                    while (dr.Read())
+                    {
+                        comboDescricao.Items.Add(dr.GetString("nome")); // Adiciona cada categoria ao ComboBox
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao carregar categorias: " + ex.Message);
+                }
+            }
+        }
+        private void btnCadastra_Click(object sender, EventArgs e)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connStr))
+            {
+                try
+                {
+                    conn.Open();
+                    string sql = "INSERT INTO lancamentos (data, tipo, categoria, descricao, valor) VALUES (@data, @tipo, @categoria, @descricao, @valor)";
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    // Define os parâmetros do comando SQL com os valores dos controles do formulário
+                    string tipo = radioEntrada.Checked ? "receita" : "despesa";
+                    cmd.Parameters.AddWithValue("@data", DateTime.Parse(txtData.Text));
+                    cmd.Parameters.AddWithValue("@tipo", tipo);
+                    cmd.Parameters.AddWithValue("@categoria", comboDescricao.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@descricao", comboDescricao.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@valor", decimal.Parse(txtValor.Text.Replace(",", ".")));
+
+                    cmd.ExecuteNonQuery(); // Executa o comando de inserção
+                    MessageBox.Show("Lançamento cadastrado com sucesso!");
+                    CarregarLancamentos(); // Atualiza o DataGridView com o novo lançamento
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao cadastrar lançamento: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Pega o ID da linha selecionada
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                DialogResult result = MessageBox.Show("Tem certeza que deseja excluir este lançamento?", "Confirmação", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    using (MySqlConnection conn = new MySqlConnection(connStr))
+                    {
+                        try
+                        {
+                            conn.Open();
+                            string sql = "DELETE FROM lancamentos WHERE id = @id";
+                            MySqlCommand cmd = new MySqlCommand(sql, conn);
+                            cmd.Parameters.AddWithValue("@id", id);
+                            cmd.ExecuteNonQuery();
+                            MessageBox.Show("Lançamento excluído!");
+                            CarregarLancamentos();
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("Erro ao excluir: " + ex.Message);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma linha para excluir.");
+            }
+        }
+
+        private void btnAtualizar_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                // Pega o ID da linha selecionada
+                int id = Convert.ToInt32(dataGridView1.SelectedRows[0].Cells["id"].Value);
+
+                using (MySqlConnection conn = new MySqlConnection(connStr))
+                {
+                    try
+                    {
+                        conn.Open();
+                        string sql = "UPDATE lancamentos SET data=@data, tipo=@tipo, categoria=@categoria, descricao=@descricao, valor=@valor WHERE id=@id";
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                        string tipo = radioEntrada.Checked ? "receita" : "despesa";
+                        cmd.Parameters.AddWithValue("@data", DateTime.Parse(txtData.Text));
+                        cmd.Parameters.AddWithValue("@tipo", tipo);
+                        cmd.Parameters.AddWithValue("@categoria", comboDescricao.SelectedItem.ToString());
+                        cmd.Parameters.AddWithValue("@descricao", comboDescricao.SelectedItem.ToString());
+                        cmd.Parameters.AddWithValue("@valor", decimal.Parse(txtValor.Text.Replace(",", ".")));
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Lançamento atualizado!");
+                        CarregarLancamentos();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro ao atualizar: " + ex.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selecione uma linha para atualizar.");
+            }
+        }
+
+        private void radioEntrada_CheckedChanged(object sender, EventArgs e)
+        {
+            CarregarCategorias(); // Carrega as categorias de receita
+        }
+
+        private void radioSaida_CheckedChanged(object sender, EventArgs e)
+        {
+            CarregarCategorias(); // Carrega as categorias de despesa
+        }
     }
 
 }
